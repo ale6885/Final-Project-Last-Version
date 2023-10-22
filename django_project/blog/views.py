4d62cebecc8d91cb0b6f2dea73b28080ee87bc54
ale6885 <alessandraseidita@gmail.com>
2023-10-22 17:33:15 +0200

    Unit 3 Part 1 completed

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts= [
+    {
+        'author': 'Alessandra',
+        'title': 'First Blog Post',
+        'content': 'Content for the first post',
+        'date_posted': '20/10/2023' 
+    },
+
+    {
+         'author': 'John',
+        'title': 'Second Blog Post',
+        'content': 'Content for the second post',
+        'date_posted': '21/10/2023' 
+    },
+
+        {
+        'author': 'Jane',
+        'title': 'Third Blog Post',
+        'content': 'Content for the third post',
+        'date_posted': '22/10/2023' 
+    }
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
-
+    context = {
+        'posts':posts 
+    }
+    return render(request, 'blog/home.html', context)
+  
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
+ 
