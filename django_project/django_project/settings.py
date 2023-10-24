9c5a145d273a5e72767ec2a3d332b74ba3745616
ale6885 <alessandraseidita@gmail.com>
2023-10-24 22:21:22 +0200

    End of week 3 + add lect

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "blog.apps.BlogConfig",
     "users.apps.UserConfig",
+    "cripsy_forms", #Updated here
+    "crispy_bootstrap5", #Updated here
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages 
+from .forms import UserRegisterForm
+
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
-    return(request,'users/register.html', {'form: form'})
+    if request.method == 'POST':
+       form = UserRegisterForm(request.POST)
+       if form.is_valid():
+          form.save()
+          username = form.cleaned_data.get('username')
+          messages.success(request, f'Account created for {username}!')
+          return redirect('blog-home')
+    else:
+        form = UserRegisterForm()    
+    return render(request,'users/register.html', {'form': form})
+
+@login_required 
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                               request.FILES,
+                               instance=request.user.profile)
+    if    
+    
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    
+    context = {
+        'u_form' : u_form,
+        'p_form' : p_form
+    }
+
+    return render(request, 'users/profile.html', context)
