99b36109e5d25a30520a27e0fb309db76d32e237
ale6885 <alessandraseidita@gmail.com>
2023-10-25 21:15:13 +0200

    End Unit 7 walk

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -11,14 +11,17 @@ def register(request):
        if form.is_valid():
           form.save()
           username = form.cleaned_data.get('username')
-          messages.success(request, f'Account created for {username}!')
-          return redirect('blog-home')
+          messages.success(request, f'Account created successfully, now you can log in.{username}!')
+          return redirect('login')
     else:
         form = UserRegisterForm()    
     return render(request,'users/register.html', {'form': form})
 
 @login_required 
 def profile(request):
+    return render(request, 'users/profile.html')
+
+
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST,
