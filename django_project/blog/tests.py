fb022b7265104dde583e34b1046ad9611f7fb5ce
ale6885 <alessandraseidita@gmail.com>
2023-11-15 08:40:17 +0000

    Begining unit12, inst coverage

--- django_project/blog/tests.py
+++ django_project/blog/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+from .models import Post 
 
 # Create your tests here.
